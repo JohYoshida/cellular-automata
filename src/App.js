6797eaada8825cfa1e0e545f1f61569edd546ff9
Joh Yoshida <johyoshida@gmail.com>
2019-06-22 18:05:37 -0700

    Check binary rule # against situation table

--- src/App.js
+++ src/App.js
@@ -69,4 +69,50 @@ function makeBinaryRule(rule) {
   }
   return binaryRule;
 }
+
+function checkRule(binaryRule, data, i) {
+  let num;
+  let left = (i === 0) ? data.length - 1 : i - 1;
+  let right = (i === data.length - 1) ? 0 : i + 1;
+  // // Check left neighbour
+  if (data[left] === 0) {
+    // Check self
+    if (data[i] === 0 ) {
+      // Check right neighbour
+      if (data[right] === 0) {
+        num = binaryRule[7];
+      } else if (data[right] === 1) {
+        num = binaryRule[6];
+      }
+    } else if (data[i] === 1) {
+      // Check right neighbour
+      if (data[right] === 0) {
+        num = binaryRule[5];
+      } else if (data[right] === 1) {
+        num = binaryRule[4];
+      }
+    }
+  } else if (data[left] === 1) {
+    // Check self
+    if (data[i] === 0 ) {
+      // Check right neighbour
+      if (data[right] === 0) {
+        num = binaryRule[3];
+      } else if (data[right] === 1) {
+        num = binaryRule[2];
+      }
+    } else if (data[i] === 1) {
+      // Check right neighbour
+      if (data[right] === 0) {
+        num = binaryRule[1];
+      } else if (data[right] === 1) {
+        num = binaryRule[0];
+      }
+    }
+  } else {
+    console.log("Error in App.js checkRule()");
+  }
+  return Number(num);
+}
+
 export default App;

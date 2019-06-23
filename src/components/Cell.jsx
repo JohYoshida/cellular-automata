919fc34e33bcafb165009783f8b41b9a11cdd26b
Joh Yoshida <johyoshida@gmail.com>
2019-06-22 18:18:34 -0700

    Conditional className

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,10 +2,23 @@ import React, { Component } from "react";
 
 class Cell extends Component {
   render() {
+    const className = makeClassName(this.props.data);
     return(
-      <div className="Cell"></div>
+      <div className={className}></div>
     )
   }
+}
+
+function makeClassName(data) {
+  let className = "Cell"
+  if (data === 0) {
+    className += " down"
+  } else if (data === 1) {
+    className += " up"
+  }
+
+
+  return className;
 }
 
 export default Cell;
